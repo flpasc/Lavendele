357c16a84a419628cb075107ec0e3fc8ad4e575a
Florian <florianpaulschoene@gmail.com>
2023-05-11 23:49:25 +0200

    seeding, migration

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,33 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Posuere imperdiet volutpat blandit sollicitudin neque class et mus sem facilisis placerat lacus, orci phasellus nam malesuada metus hendrerit cursus suscipit venenatis turpis dui hac, etiam amet varius faucibus aliquam maximus vivamus quam nostra augue curae.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Posuere imperdiet volutpat blandit sollicitudin neque class et mus sem facilisis placerat lacus, orci phasellus nam malesuada metus hendrerit cursus suscipit venenatis turpis dui hac, etiam amet varius faucibus aliquam maximus vivamus quam nostra augue curae.'
-            ]
-        ];
-    }
-
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
