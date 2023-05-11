a8c2ab945e6687ced3aa99f240ef0f7c5dc95575
Florian <florianpaulschoene@gmail.com>
2023-05-11 23:55:51 +0200

    db fix wrong naming

--- database/migrations/2023_05_11_211113_create_listings_table.php
+++ database/migrations/2023_05_11_211113_create_listings_table.php
@@ -16,9 +16,8 @@ return new class extends Migration
             $table->string('title');
             $table->string('tags');
             $table->string('company');
-            $table->string('location');
-            $table->string('strings');
             $table->string('email');
+            $table->string('location');
             $table->string('website');
             $table->longText('description');
             $table->timestamps();
